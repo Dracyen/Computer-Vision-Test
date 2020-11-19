d2a647211ae34178f94863e9e2857b061cdba04d
CarlosCristalli <56305091+CarlosCristalli@users.noreply.github.com>
2020-11-19 15:46:27 +0000

    Basic Gameplay

---
+++ Assets/MapCreator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreator : MonoBehaviour
+{
+    public GameObject[] road;
+    GameObject backRoad;
+    GameObject middleRoad;
+    GameObject frontRoad;
+    Vector3 instantiatePos = new Vector3(30, 0, 0);
+    public Player_Move Player;
+    bool build = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Move>();
+        backRoad = Instantiate(road[0], new Vector3(-30, 0, 0), Quaternion.identity);
+        middleRoad = Instantiate(road[0], new Vector3(0, 0, 0), Quaternion.identity);
+        frontRoad = Instantiate(road[0], instantiatePos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Player.Hit && !build)
+        {
+            Rebuild();
+            build = true;
+        }
+        if (!Player.Hit)
+        {
+            build = false;
+        }
+    }
+
+    void Rebuild()
+    {
+        Debug.Log("Player");
+        instantiatePos = instantiatePos + new Vector3(30, 0, 0);
+        Destroy(backRoad);
+        backRoad = middleRoad;
+        middleRoad = frontRoad;
+        frontRoad = Instantiate(road[Random.Range(0,road.Length)], instantiatePos, Quaternion.identity);
+    }
+    
+}
---
+++ Assets/Player_Life.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Life : MonoBehaviour
+{
+    public Slider Life;
+    public Image LifeColor;
+    int PlayerLife;
+    int PlayerMaxLife = 100;
+    int damege = 25;
+    public GameObject EndScreen;
+    bool canTakeDamege = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerLife = PlayerMaxLife;
+    }
+    IEnumerator HitCoolDown()
+    {
+        canTakeDamege = false;
+        yield return new WaitForSeconds(0.7f);
+        canTakeDamege = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerLife > 50)
+        {
+            LifeColor.color = Color.green;
+        }
+        if (PlayerLife == 50)
+        {
+            LifeColor.color = Color.yellow;
+        }
+        if (PlayerLife == 25)
+        {
+            LifeColor.color = Color.red;
+        }
+        Life.value = PlayerLife;
+        if (PlayerLife <= 0)
+        {
+            this.gameObject.SetActive(false);
+            EndScreen.gameObject.SetActive(true);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "Barrier" && canTakeDamege)
+        {
+            StartCoroutine("HitCoolDown");
+            PlayerLife -= damege;
+        }
+
+    }
+}
