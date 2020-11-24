3d951e97783953a45cc16f21f602b2952ef6b142
CarlosCristalli <56305091+CarlosCristalli@users.noreply.github.com>
2020-11-24 14:37:46 +0000

    Animation

--- Assets/_Vitor/strafe.cs
+++ Assets/_Vitor/strafe.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class strafe : MonoBehaviour
 {
+    Animator anim;
+    Vector3 LastPos;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     public void changePosition(int currZ, int sWidth, float rWidth)
     {
         float position = currZ * rWidth / sWidth;
-
+        LastPos = transform.position;
         transform.position = new Vector3(transform.position.x, transform.position.y, position);
+        if(LastPos.z > transform.position.z)
+        {
+            anim.SetBool("TurnR", false);
+            anim.SetBool("TurnL", true);
+        }
+        if (LastPos.z < transform.position.z)
+        {
+            anim.SetBool("TurnR", true);
+            anim.SetBool("TurnL", false);
+        }
+        if (LastPos.z == transform.position.z)
+        {
+            anim.SetBool("TurnR", false);
+            anim.SetBool("TurnL", false);
+        }
     }
 }
